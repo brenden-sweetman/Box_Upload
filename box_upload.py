88a5c2249d6602aef7c46845c7718f7b420369ab
Brenden Sweetman <brenden.sweetman@wustl.edu>
2018-03-02 12:43:58 -0600

    added full function (mvp)

--- box_upload.py
+++ box_upload.py
@@ -1,67 +1,133 @@
+#!/usr/bin/env python
 from boxsdk import OAuth2
 from boxsdk import Client
+from boxsdk import exception
 import requests
 import time
+import re
+import sys , os
+import ntpath
+import argparse
+from StringIO import StringIO
 
-clientID="pmg46vz4x71qkvxsg3p90r35q75s961q"
-clientSecret="4gyRHY7qd9ttsukDnrJCp5gvKG97Dvw7"
-#refreshToken="NpQE6mACg5eaCY5nYZsHzHBkg9f9BAan3x4MQdPFg7BqbuSZq2AciYaCizJqPWh2"
+clientID=""
+clientSecret=""
 refreshToken=""
 accessToken=""
 expireTime=0
+# ID for folder to upload files
+folderID=46200064078
 oauth = None
-verbose=True
+client = None
+verbose= False
+
+
+### Main function ###
+def main():
+    global client, oauth, verbose
+    #parse command line arguments
+    parser = argparse.ArgumentParser(description="Upload File to box")
+    parser.add_argument("-v","--verbose", help="activate verbose mode", action='store_true')
+    parser.add_argument("-f","--file", help="path to upload file", type=str, required=True)
+    args=parser.parse_args(sys.argv[1:])
+    verbose=args.verbose
+    # read config
+    readCfg()
+    # updtae auth if needed
+    if expireTime<time.time():
+        refreshAuth()
+        writeCfg()
+    # Autenticate with box
+    oauth= OAuth2(client_id=clientID,client_secret=clientSecret,access_token=accessToken)
+    # Create client
+    client=Client(oauth)
+    # Upload file
+    upload(args.file)
+
+def upload(filePath,folder=folderID):
+    global client, verbose
+    try:
+        print "Starting Upload to box...\n\n"
+        box_file=client.folder(str(folder)).upload(filePath,ntpath.basename(filePath))
+        if verbose:
+            print "Uploaded file to box"
+            print "File Name: "+ box_file.name
+            print "File ID: " + box_file.id
+            print "In Folder:"
+            print "\tName: " + client.folder(str(folder)).get()['name']
+            print "\tID: " + str(folder)
+    except exception.BoxAPIException as e:
+        print "Error uploading file: " + e.message
 
 def refreshAuth():
-    global clientID, clientSecret, refreshToken, accessToken, expireTime, startTime, verbose
+    global clientID, clientSecret, refreshToken, accessToken, expireTime , folderID, verbose
+    ### Request Global Acess Token ###
+    # prepare authentication request (see: https://developer.box.com/reference#oauth-2-overview)
     refreshPayload={
         'grant_type':'refresh_token',
         'refresh_token':refreshToken,
         'client_id':clientID,
         'client_secret':clientSecret}
-    print refreshPayload
+    # send post request
     response= requests.post("https://api.box.com/oauth2/token",refreshPayload)
-    responseChunks= response.text.replace('{',"").replace('}',"").replace('\"',"").split(",")
-    for s in responseChunks:
-        chunks=s.split(":")
-        if chunks[0]=="access_token":
-            accessToken=chunks[1].replace(' ',"")
-        if chunks[0]=="expires_in":
-            expireTime= time.time()+int(chunks[1])
-        if chunks[0]=="refresh_token":
-            refreshToken=chunks[1]
+    # parse response
+    if response.status_code==200:
+        print response.text
+        accessToken= re.findall(r"access_token\":\"([^\"]+)\"",response.text)[0]
+        refreshToken= re.findall(r"refresh_token\":\"([^\"]+)\"",response.text)[0]
+    # print error if bad request
+    else: print "Access Token could not be created. Use verbose mode for more details"
     if verbose:
         print "Refresh Auth response:"
         print "Status: "+ str(response.status_code)
         print "Access Token: " +accessToken
-        print "Refresh Token:" +refreshToken
-        print "Access Token Experation:" + str(expireTime)
-    writeCfg()
+        print "Refresh Token: " +refreshToken
+        print "\n\n"
+    ### DownScope Access Token ###
+    # prepare downscope request (see:https://developer.box.com/reference#token-exchange)
+    payload={
+        'subject_token' : accessToken,
+        'subject_token_type' : 'urn:ietf:params:oauth:token-type:access_token',
+        'scope' : 'item_upload',
+        'resource' : 'https://api.box.com/2.0/folders/'+str(folderID),
+        'grant_type' : 'urn:ietf:params:oauth:grant-type:token-exchange'
+    }
+    # send post request
+    response= requests.post("https://api.box.com/oauth2/token",payload)
+    # parse result
+    if response.status_code==200:
+        accessToken= re.findall(r"access_token\":\"([^\"]+)\"",response.text)[0]
+        expireTime= int(re.findall(r"expires_in\":(\d+)",response.text)[0])+ time.time()
+    # print error for bad response
+    else: print "Downscope Access Token cound not be created. Use verbose mode for more details"
+    if verbose:
+        print "DownScope Auth response"
+        print "Status: "+ str(response.status_code)
+        print "Acces Token:" + accessToken
+        print "Expreation Time:" + str(expireTime)
+        print "\n\n"
 
+
+### Write all required ID's and Tokens to a configuration file ###
 def writeCfg():
     global clientID, clientSecret, refreshToken, accessToken, expireTime
-    with open("box.cfg", 'w') as cfg:
+    with open("/Users/brenden/Desktop/git/Box_Upload/box.cfg", 'w') as cfg:
         cfg.write(clientID+"\n")
         cfg.write(clientSecret+"\n")
         cfg.write(refreshToken+"\n")
         cfg.write(accessToken+"\n")
         cfg.write(str(expireTime)+"\n")
 
+### Read all required ID's and Tokens from configuration file ###
 def readCfg():
     global clientID, clientSecret, refreshToken, accessToken, expireTime
-    with open ("box.cfg", 'r') as cfg:
+    with open ("/Users/brenden/Desktop/git/Box_Upload/box.cfg", 'r') as cfg:
         clientID= cfg.readline().replace("\n","")
         clientSecret= cfg.readline().replace("\n","")
         refreshToken= cfg.readline().replace("\n","")
         accessToken= cfg.readline().replace("\n","")
         expireTime= float(cfg.readline().replace("\n",""))
 
-readCfg()
-if expireTime<time.time():
-    refreshAuth()
-oauth= OAuth2(client_id=clientID,client_secret=clientSecret,access_token=accessToken)
-client=Client(oauth)
-root_items = client.folder(folder_id='0').get_items(limit=100,offset=0)
-for item in root_items:
-    print item["name"]
-    print item["id"]
+# call main function
+if __name__=="__main__":
+    main()
--- reauth.py
+++ reauth.py
@@ -1,18 +1,32 @@
+#!/usr/bin/env python
 import requests
+import re
+import time
 
-#request url: https://account.box.com/api/oauth2/authorize?response_type=code&client_id=pmg46vz4x71qkvxsg3p90r35q75s961q
-
-def writeCfg():
-    global clientID, clientSecret, refreshToken, accessToken, expireTime
-    with open("box.cfg", 'w') as cfg:
-        cfg.write(clientID+"\n")
-        cfg.write(clientSecret+"\n")
-        cfg.write(refreshToken+"\n")
-        cfg.write(accessToken+"\n")
-        cfg.write(str(expireTime)+"\n")
-
-
-
-
-request= requests.post("https://api.box.com/oauth2/token", {"grant_type":"authorization_code","code":"KON9I7idVGLUwjvFnukqjFhngVfo6md3","client_id":"pmg46vz4x71qkvxsg3p90r35q75s961q","client_secret":"4gyRHY7qd9ttsukDnrJCp5gvKG97Dvw7"})
-print request.text
+print "This Script Authenticates the box_upload.py application to access your box account"
+print "Enter your client ID:"
+clientID=raw_input()
+print "Enter your client secret"
+clientSecret=raw_input()
+print "Type the following url into a browser login to your account and paste the code generated below"
+print "https://account.box.com/api/oauth2/authorize?response_type=code&client_id="+clientID
+code=raw_input()
+payload={
+    "grant_type":"authorization_code",
+    "code":code,
+    "client_id":clientID,
+    "client_secret":clientSecret
+}
+response = requests.post("https://api.box.com/oauth2/token",payload)
+if response.status_code==200:
+    accessToken= re.findall(r"access_token\":\"([^\"]+)\"",response.text)[0]
+    refreshToken= re.findall(r"refresh_token\":\"([^\"]+)\"",response.text)[0]
+    expireTime= int(re.findall(r"expires_in\":(\d+)",response.text)[0])+ time.time()
+# print error if bad request
+else: print "Access Token could not be created."
+with open("box_initial.cfg", 'w') as cfg:
+    cfg.write(clientID+"\n")
+    cfg.write(clientSecret+"\n")
+    cfg.write(refreshToken+"\n")
+    cfg.write(accessToken+"\n")
+    cfg.write(str(expireTime)+"\n")
