7635852f6e53a619f05c40462f91ce42fcf83fe9
Brenden Sweetman <brenden.sweetman@wustl.edu>
2018-04-06 13:02:59 -0500

    documentation

--- box_upload.py
+++ box_upload.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Author: Brenden Sweetman brenden.sweetman@wustl.edu
 # Title: box_upload
 # Purpose: Upload files to a given folder in wustl box
 #   The script can be used generally given the -i (--ID) argument to specify a different location in box
-#   however, generally the script will upload to the designated file to share directly with the teaching clientSecret
+#   however, generally the script will upload to the designated file to share directly with the teaching center
+#   Please see the technical documentation for information on how the script works
 #   The script was developed using the box python api (https://github.com/box/box-python-sdk)
 
 from boxsdk import OAuth2
@@ -22,7 +23,7 @@ refreshToken=""
 accessToken=""
 expireTime=0
 # ID for folder to upload files
-folderID=47434826943
+folderID=0
 oauth = None
 client = None
 verbose= False
@@ -31,42 +32,48 @@ conf_path = ""
 
 ### Main function ###
 def main():
-    global client, oauth, verbose, conf_path
+    global client, oauth, verbose, conf_path, folderID
     #parse command line arguments
     parser = argparse.ArgumentParser(description="Upload File to box")
     parser.add_argument("-v","--verbose", help="activate verbose mode", action='store_true')
     parser.add_argument("-f","--file", help="path to upload file", type=str, required=True)
     parser.add_argument("-c","--config", help="path to config file", type=str, required=True)
+    parser.add_argument("-i","--ID",help="Provide alternative box file ID for upload",type=int)
     args=parser.parse_args(sys.argv[1:])
+    # Pull values from command line args
     verbose=args.verbose
     conf_path = args.config
-    # read config
+    if args.ID!= None:
+        folderID = args.ID
+    # read config file
     readCfg()
-    # updtae auth if needed
+    # update auth if needed
     if expireTime<time.time():
         refreshAuth()
         writeCfg()
-    # Autenticate with box
+    # Authenticate with box
     oauth= OAuth2(client_id=clientID,client_secret=clientSecret,access_token=accessToken)
     # Create client
     client=Client(oauth)
     # Upload file
     upload(args.file)
 
-def upload(filePath,folder=folderID):
-    global client, verbose
+def upload(filePath):
+    global client, verbose, folderID
+    # try to upload file
+    print(folderID)
     try:
-        print "Starting Upload to box...\n\n"
-        box_file=client.folder(str(folder)).upload(filePath,ntpath.basename(filePath))
+        print ("Starting Upload to box...\n\n")
+        box_file=client.folder(str(folderID)).upload(filePath,ntpath.basename(filePath))
         if verbose:
-            print "Uploaded file to box"
-            print "File Name: "+ box_file.name
-            print "File ID: " + box_file.id
-            print "In Folder:"
-            print "\tName: " + client.folder(str(folder)).get()['name']
-            print "\tID: " + str(folder)
+            print ("Uploaded file to box")
+            print ("File Name: "+ box_file.name)
+            print ("File ID: " + box_file.id)
+            print ("In Folder:")
+            print ("\tName: " + client.folder(str(folderID)).get()['name'])
+            print ("\tID: " + str(folderID))
     except exception.BoxAPIException as e:
-        print "Error uploading file: " + e.message
+        print ("Error uploading file: " + e.message)
 
 def refreshAuth():
     global clientID, clientSecret, refreshToken, accessToken, expireTime , folderID, verbose
@@ -84,13 +91,13 @@ def refreshAuth():
         accessToken= re.findall(r"access_token\":\"([^\"]+)\"",response.text)[0]
         refreshToken= re.findall(r"refresh_token\":\"([^\"]+)\"",response.text)[0]
     # print error if bad request
-    else: print "Access Token could not be created. Use verbose mode for more details"
+    else: print ("Access Token could not be created. Use verbose mode for more details")
     if verbose:
-        print "Refresh Auth response:"
-        print "Status: "+ str(response.status_code)
-        print "Access Token: " + accessToken
-        print "Refresh Token: " + refreshToken
-        print "\n\n"
+        print ("Refresh Auth response:")
+        print ("Status: "+ str(response.status_code))
+        print ("Access Token: " + accessToken)
+        print ("Refresh Token: " + refreshToken)
+        print ("\n\n")
     ### DownScope Access Token ###
     # prepare downscope request (see:https://developer.box.com/reference#token-exchange)
     payload={
@@ -107,13 +114,13 @@ def refreshAuth():
         accessToken= re.findall(r"access_token\":\"([^\"]+)\"",response.text)[0]
         expireTime= int(re.findall(r"expires_in\":(\d+)",response.text)[0])+ time.time()
     # print error for bad response
-    else: print "Downscope Access Token could not be created. Use verbose mode for more details"
+    else: print ("Downscope Access Token could not be created. Use verbose mode for more details")
     if verbose:
-        print "DownScope Auth response"
-        print "Status: "+ str(response.status_code)
-        print "Acces Token:" + accessToken
-        print "Expreation Time:" + str(expireTime)
-        print "\n\n"
+        print ("DownScope Auth response")
+        print ("Status: "+ str(response.status_code))
+        print ("Access Token:" + accessToken)
+        print ("Expiration Time:" + str(expireTime))
+        print ("\n\n")
 
 
 ### Write all required ID's and Tokens to a configuration file ###
